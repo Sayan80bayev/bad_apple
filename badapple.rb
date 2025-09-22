432bce367cfbef285d20135452938c84c6fb792f
sayanseksenbaev <sayan123serv@gmail.com>
2025-09-22 18:39:44 +0500

    Fix: full dir compile

--- badapple.rb
+++ badapple.rb
@@ -2,7 +2,7 @@ class Badapple < Formula
   desc "ASCII Bad Apple player for terminal"
   homepage "https://github.com/sayan80bayev/homebrew-badapple"
   url "https://github.com/sayan80bayev/homebrew-badapple/releases/download/v1.0.1/badapple-macos-x86_64.tar.gz"
-  sha256 "3008af0db0201c01d57ecc8259da629ea4bb4f56334dd4dc4aecac80a3c1db24"
+  sha256 "4ce17e79de06918ceaff4ccee210ebb823f0cd9b78cd778c96eb3bd242291104"
   license "MIT"
 
   def install
