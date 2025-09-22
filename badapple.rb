3779541a8118a96fa96bf531826186e3f0079800
sayanseksenbaev <sayan123serv@gmail.com>
2025-09-22 18:13:27 +0500

    Test: updating the url

--- badapple.rb
+++ badapple.rb
@@ -1,7 +1,7 @@
 class Badapple < Formula
   desc "ASCII Bad Apple player for terminal"
   homepage "https://github.com/sayan80bayev/badapple"
-  url "https://github.com/sayan80bayev/homebrew-badapple/releases/download/v1.0.0/badapple-macos-x86_64.tar.gz"
+  url "https://github.com/user-attachments/files/22466688/badapple-macos-x86_64.tar.gz"
   sha256 "3008af0db0201c01d57ecc8259da629ea4bb4f56334dd4dc4aecac80a3c1db24"
   license "MIT"
 
